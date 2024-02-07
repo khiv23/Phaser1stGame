68b5b85883b85c58650a6b5fedbe233a9cded26a
khiv23 <158025412+khiv23@users.noreply.github.com>
2024-02-07 11:54:48 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,41 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+ 
+
+function update ()
+{
+}}
